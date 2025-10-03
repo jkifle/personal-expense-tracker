c030d2f2e90a903ad6e4a4d718855576f59bdaa5
Joseph Kifle <kifle.joe@gmail.com>
2025-10-03 12:52:50 -0400

    vite

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,13 @@ import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
     plugins: [react()],
+    optimizeDeps: {
+        // Force Vite to prebundle 'cookie'
+        include: ['cookie'],
+    },
+    ssr: {
+        // Prevent Vite from treating 'cookie' as an external dependency
+        noExternal: ['cookie'],
+    },
 });
+// vite.config.js
