7646d45109d0f948ad98390f578252098eb36711
Joseph Kifle <kifle.joe@gmail.com>
2025-10-04 15:11:26 -0500

    modification to env

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,24 +1,18 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore';
-import { getAuth } from "firebase/auth"
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: "personal-expense-tracker-f587f.firebaseapp.com",
-  projectId: "personal-expense-tracker-f587f",
-  storageBucket: "personal-expense-tracker-f587f.firebasestorage.app",
-  messagingSenderId: "369613308634",
-  appId: "1:369613308634:web:7d6ef939623f1227ee5be3"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app)
+const auth = getAuth(app);
 const db = getFirestore(app);
-
 
 export { db, app, auth };
